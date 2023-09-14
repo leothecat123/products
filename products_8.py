0550a91dc8a8128bc3788ccfa08546b3b8bb321b
leo lam <raft-a-hill@leodeMac-mini.local>
2023-09-14 18:54:05 +0800

    add check file existance function

---
+++ products_8.py
@@ -0,0 +1,38 @@
+#operating system
+import os
+
+#檢查檔案在不在，如果有就讀取
+products = []
+if os.path.isfile('products.csv'):
+    print('Yes, file existed')
+    with open('products.csv', 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+
+else:
+    print('No, file not found')
+
+
+#讓使用者輸入
+while True:
+    name = input('請輸入商品名稱：')
+    if name == 'q':
+        break
+    price = input('請輸入商品價格：')
+    price = int(price)
+    products.append([name, price])
+print(products)
+
+#印出所有購買紀錄
+for p in products:
+    print(p[0], '的價格是', '$', p[1])
+
+#寫入檔案
+with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('商品,價格\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
